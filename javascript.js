9295e53197e2e2779b9d5a3ca054aede526b69f1
kehughes96 <kehnt7@mail.missouri.edu>
2016-11-28 21:42:59 -0600

    Figured out how to keep the menu horizontal!

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 //When window is resized, the display:none is turned off for the ul so the menu displays on click or touch.
   // $(window).resize(function() {
 	//    if (  $(window).width() > 700 ) {
-	// 	$('.mobile-nav').;
+	// 	$('.mobile-nav').removeAttr('style');
 	//  }
 	// });//end resize
 });//end ready
