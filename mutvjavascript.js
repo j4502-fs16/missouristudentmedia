7dc01f271e4ac65b35000f5e5814e288b8ac2d7a
ToddChen <ycq78@mail.missouri.edu>
2016-12-10 23:03:15 -0600

    gallery

--- mutvjavascript.js
+++ mutvjavascript.js
@@ -10,3 +10,14 @@ $(document).ready(function() {
 	 }
 	});//end resize
 });//end ready
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
